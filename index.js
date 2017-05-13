10138bcba1ef872b6a273c94c5cd6a344e9dd8da
chao <dc10101@qq.com>
2017-05-13 11:43:43 +0800

    README

--- index.js
+++ index.js
@@ -1,64 +1,86 @@
-module.exports = function (options) {
-  const ms = options.ms
-  const limit = options.limit
-  const keyFn = options.keyFn
-  const onReachLimit = options.onReachLimit
-
-  if (!ms) {
-    throw new Error('Missing options: ms')
+function limitInRange(val, min, max) {
+  if (val > max) {
+    return max
   }
-  if (!limit) {
-    throw new Error('Missing options: limit')
+  if (val < min) {
+    return min
   }
+  return val | 0 // to integer, floor
+}
 
-  const history = {}
-
-  function debug() {
-    console.log(JSON.stringify(history, ' ', 4))
-  }
-
-  setInterval(function () {
-    let now = Date.now()
-    for (let key in history) {
-      if (now - history[key].lastAccess > ms) {
-        delete history[key]
-      }
+module.exports = function (defaultOptions) {
+  return function (options) {
+    let opt = Object.assign({}, defaultOptions, options)
+    let {ms, limit, keyFn, onLimit, qps} = opt
+    if (!ms) {
+      throw new Error('Missing options: ms')
     }
-  }, 1000 * 60 * 10) // clean very 10min
+    if (!limit) {
+      throw new Error('Missing options: limit')
+    }
 
-  return (req, res, next) => {
-    let key = typeof keyFn === 'function' ? keyFn(req) : '*'
-    let now = Date.now()
-    let host = history[key]
+    const history = {}
+    const maxInterval = 1000 * 60 * 5 // 5 min
+    const minInterval = 1000 * 5 // 5 sec
+    const bestWithinCount = 5000 // if more than 5000 to clear, maybe hang the request
+    let gcInterval = qps ? bestWithinCount / qps * 1000 : minInterval
+    gcInterval = limitInRange(gcInterval, minInterval, maxInterval)
 
-    if (host) {
-      if(options.debug){
-        debug()
-      }
-      let list = host.list
-      if (list.length >= limit && now - list[0] < ms) {
-        if(typeof onReachLimit === 'function'){
-          return onReachLimit(req, res)
+    let cleanHistory = () => {
+      let now = Date.now()
+      let removedCount = 0
+
+      for (let key in history) {
+        let list = history[key]
+        let lastTimestamp = list[list.length - 1]
+        if (now - lastTimestamp > ms) {
+          removedCount += list.length
+          delete history[key]
         }
-        let err = new Error('Too many requests')
-        err.status = 429
-        return next(err)
+      }
+
+      if (!qps) { // not specify QPS, use smart-estimate
+        gcInterval = gcInterval / ((removedCount + 1) / bestWithinCount) // if removedCount == 0, + 1 to avoid divided by zero error
+        gcInterval = limitInRange(gcInterval, minInterval, maxInterval)
+      }
+      if (opt.debug) {
+        console.log(`limit[${opt.name}] GC! removedCount=${removedCount}, gcInterval=${gcInterval}`)
+      }
+      setTimeout(cleanHistory, gcInterval)
+    }
+
+    setTimeout(function () {
+      cleanHistory()
+    }, gcInterval)
+
+    return (req, res, next) => {
+      let key = typeof keyFn === 'function' ? keyFn(req) : '*'
+      let now = Date.now()
+      let list = history[key]
+      if(opt.debug){
+        console.log(`limit[${opt.name}] request! gcInterval=${gcInterval}: `, JSON.stringify(history, ' ', 4))
+      }
+      if (list) {
+        if (list.length >= limit && now - list[0] < ms) { // should block
+          if (typeof onLimit === 'function') {
+            return onLimit(req, res)
+          }
+          let err = new Error('Too many requests')
+          err.status = 429
+          return next(err)
+        }
+        else {
+          if (list.length === limit) {
+            list.shift()
+          }
+          list.push(now)
+          return next()
+        }
       }
       else {
-        if (list.length >= limit) {
-          list.shift()
-        }
-        list.push(now)
-        host.lastAccess = now
-        return next()
+        history[key] = [now]
+        next()
       }
-    }
-    else {
-      history[key] = {
-        lastAccess: now,
-        list: [now]
-      }
-      next()
     }
   }
 }
